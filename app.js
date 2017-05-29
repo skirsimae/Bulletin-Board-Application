eca20b6deba572c446a08eae47122040abd2f7ac
Silva Kirsimae <skirsimae@tmk.edu.ee>
2017-05-29 16:26:10 +0200

    styling added

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ const pg = require('pg'); //include node postgres library.
 app.use(bodyParser.urlencoded({
     extended: false
 }))
-app.set('views', './src/views');
+app.use(express.static('public'));
+
+app.set('views', './public/views');
 app.set('view engine', 'pug');
 
 var connectionString = 'postgres://' + process.env.POSTGRES_USER + ':' + process.env.POSTGRES_PASSWORD + '@localhost/bulletinboard';
@@ -17,20 +19,18 @@ app.get('/', function(req, res) {
 });
 
 app.post('/', function(req,res) {
-	pg.connect( connectionString, function(err, client, done) { //connects to the database of 'messages'; the callback: if no connection possible with the database - error; client - require the database and request a query; after the result of the query is done, done callback happens. 
+	pg.connect( connectionString, function(err, client, done) { //connect to the database. 
 		if(err){ //handle connection errors. 
 			throw err;
 		}
 		done();
 
 		client.query('INSERT INTO messages(title, body) values($1,$2)', [req.body.title, req.body.body]); //SQL Query --> insert data.
-		// client.query('INSERT INTO messages(title, body) values(${title}, ${body})', [req.body.title, req.body.body]); //SQL Query --> insert data.
-
 		done();
 
-			res.redirect('/all_messages')
-		});
+			res.redirect('all_messages')
 	});
+});
 
 app.get('/all_messages', function(req,res) {
 	pg.connect(connectionString, function(err, client, done) {
